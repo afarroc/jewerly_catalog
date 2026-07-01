eca9cd2f23baa7c34abcd05ec41eae341499ad67
afarroc <108610976+afarroc@users.noreply.github.com>
2026-06-30 20:15:15 -0500

    Fix syntax error in manage.py and remaining Jewelry typo

--- manage.py
+++ manage.py
@@ -6,8 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelry
-jewelry_catalog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelry_catalog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
